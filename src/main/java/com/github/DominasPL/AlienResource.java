5e22cc2082b6cbf9cddd2f372727a36acd6ad840
DomiasPL <dominik-stepuch@wp.pl>
2019-08-11 16:48:58 +0200

    Added JSON formatand test it via Postman

--- src/main/java/com/github/DominasPL/AlienResource.java
+++ src/main/java/com/github/DominasPL/AlienResource.java
@@ -4,7 +4,6 @@ import com.github.DominasPL.models.Alien;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.util.Arrays;
 import java.util.List;
 
 @Path("aliens")
@@ -13,7 +12,7 @@ public class AlienResource {
     private AlienRepository alienRepository = new AlienRepository();
 
     @GET
-    @Produces(MediaType.APPLICATION_XML)
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public List<Alien> getAliens() {
 
         return alienRepository.getAliens();
@@ -21,7 +20,7 @@ public class AlienResource {
 
     @GET
     @Path("alien/{id}")
-    @Produces(MediaType.APPLICATION_XML)
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Alien getAlien(@PathParam("id") int id) {
 
         return alienRepository.getAlien(id);
@@ -32,6 +31,7 @@ public class AlienResource {
     public Alien createAlien(Alien alien) {
 
         alienRepository.create(alien);
+        System.out.println(alien);
 
         return alien;
     }
