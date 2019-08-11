295c4877eaf647b76c1aeade6d97735f0cdd9d12
DomiasPL <dominik-stepuch@wp.pl>
2019-08-11 14:54:59 +0200

    Created endpoint to display all aliens

--- src/main/java/com/github/DominasPL/AlienResource.java
+++ src/main/java/com/github/DominasPL/AlienResource.java
@@ -6,21 +6,30 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Arrays;
+import java.util.List;
 
 @Path("aliens")
 public class AlienResource {
 
     @GET
     @Produces(MediaType.APPLICATION_XML)
-    public Alien getAlien() {
+    public List<Alien> getAliens() {
 
         System.out.println("getAlien() called!!");
         Alien a1 = new Alien();
         a1.setName("Predator");
         a1.setPoints(100);
 
-        return a1;
+        Alien a2 = new Alien();
+        a2.setName("Dominas");
+        a2.setPoints(50);
+
+
+        return Arrays.asList(a1,a2);
     }
+
+
 
 
 }
